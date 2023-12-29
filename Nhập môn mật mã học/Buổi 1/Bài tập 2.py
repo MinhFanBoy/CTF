88d335d71a3a7091456f7524cbf43a3979b2feac
Minh <hien19092005@gmail.com>
2023-12-29 15:04:59 +0700

    Create Bài tập 2.py

---
+++ Nhập môn mật mã học/Buổi 1/Bài tập 2.py
@@ -0,0 +1,19 @@
+k = [[5, 8], [12, 7]]
+
+enc = "QQCD"
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+det = (5 * 7 - 8 * 12)
+inv_det = pow(det, -1, len(alphabet))
+inv_k = [[7, -8], [-12, 5]]
+
+def de_Hill(txt):
+    flag = ""
+    for x in range(0, len(txt), 2):
+        p = [alphabet.index(txt[x]) * inv_det, alphabet.index(txt[x + 1]) * inv_det]
+        
+        tmp = [(p[0]*inv_k[0][0] + p[1] * inv_k[1][0]) % len(alphabet), (p[0]*inv_k[0][1] + p[1] * inv_k[1][1]) % len(alphabet)]
+        flag += alphabet[tmp[0]] + alphabet[tmp[1]]
+        
+    return flag
+
+print(de_Hill(enc))
