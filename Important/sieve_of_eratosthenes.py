cbc12dec213ac5b9f80a5cd3eba230041309fa6d
Minh <hien19092005@gmail.com>
2024-08-23 19:36:53 +0700

    Update sieve_of_eratosthenes.py

--- Important/sieve_of_eratosthenes.py
+++ Important/sieve_of_eratosthenes.py
@@ -1,13 +1,13 @@
 
-def sieve_of_eratosthenes(bot_bit, up_bit):
-    lower_limit = 2**(bot_bit)
-    upper_limit = 2**(up_bit) 
-    primes = [True] * (upper_limit - lower_limit)
+def erato(n):
 
-    for i in range(2, int(upper_limit ** 0.5) + 1):
-        if primes[i]:
-            for j in range(max(i*i, (lower_limit + i - 1) // i * i), upper_limit, i):
-                primes[j - lower_limit] = False
+    arr = {}
+    for i in range(2, n):
+        arr[i] = True
 
-    prime_numbers = [i for i in range(lower_limit, upper_limit) if primes[i - lower_limit]]
-    return prime_numbers
+    for i in range(2, ceil(sqrt(n))):
+        if arr[i]:
+            for j in range(i**2, n, i):
+                arr[j] = False
+
+    return [i for i in range(2, n) if (arr[i] and int(i).bit_length() == (int(n).bit_length() - 1))]
